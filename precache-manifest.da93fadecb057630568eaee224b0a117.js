44f226b200938fc2a91f722f32a39487b039e861
gour-io <you@example.com>
2020-04-20 10:39:41 +0530

    Updates

---
+++ precache-manifest.da93fadecb057630568eaee224b0a117.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f8dbad8da599ae1d771917135ce9ef",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "a27699cd33fc26fb5b9f",
+    "url": "/corona-tracker/static/css/main.96166bd3.chunk.css"
+  },
+  {
+    "revision": "c2db4c4e14d0e9a6b201",
+    "url": "/corona-tracker/static/js/2.0d677ef0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.0d677ef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27699cd33fc26fb5b9f",
+    "url": "/corona-tracker/static/js/main.dfdedb89.chunk.js"
+  },
+  {
+    "revision": "65d201989fb8ccbbe613",
+    "url": "/corona-tracker/static/js/runtime-main.23396cc5.js"
+  },
+  {
+    "revision": "aa63ccf157ea3ae16e9365c2d2c787b0",
+    "url": "/corona-tracker/static/media/covid19.aa63ccf1.png"
+  }
+]);
